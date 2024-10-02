b8b0e18f032f8e48811f1faa1d7bb0b8d1359efb
tenor2000 <tenor2000@gmail.com>
2024-10-02 13:52:13 -0400

    fixed emailjs endpoint

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import emailjs from '@emailjs/nodejs'; // v4.1.0 not working, currently using v2.2.0
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+const serviceId = process.env.EMAILJS_SERVICE_ID;
+const templateId = process.env.EMAILJS_TEMPLATE_ID;
+const publicKey = process.env.EMAILJS_PUBLIC_KEY;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/send-email', (req, res) => {
-    const { service_id, template_id, user_id, template_params } = req.body;
+    const { name, email, message } = req.body;
 
-    const emailjs = require('emailjs-com');
-    emailjs.send(service_id, template_id, template_params, user_id)
+    const templateParams = {
+        from_name: name,
+        from_email: email,
+        message: message,
+    };
+
+    emailjs.init({ publicKey: publicKey});
+
+    emailjs.send(serviceId, templateId, templateParams, publicKey)
         .then(response => {
             res.json({ success: true, message: 'Email sent successfully!' });
         })
